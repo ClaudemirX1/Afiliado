7ff6c64ddf1ce73a357eeaea9ad5e0b97d77de89
Claudemir santos <claudemirplis111@gmail.com>
2026-01-31 17:26:38 -0300

    a16

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
 const produtos = [
+
   {
-    nome: "Relógio Inteligente",
-    descricao: "Smartwatch compatível com Android e iOS",
-    preco: "R$ 149,90",
-    categoria: "eletronicos",
-    plataforma: "shopee",
-    link: "https://shopee.com.br/SEU-LINK-AFILIADO",
-    imagem: "https://raw.githubusercontent.com/ClaudemirX1/Imagem/refs/heads/main/relogio.jpg?token=GHSAT0AAAAAADUPKSUV7TGI4P3SJM3FC2CQ2L6IHWQ"
-  },
-  {
-    nome: "Fone Bluetooth",
-    descricao: "Som potente e bateria potente",
-    preco: "R$ 89,90",
+    nome: "Samsung Galaxy A16 128GB 4GB RAM Câmera 50MP Tela 6.7 NFC Preto",
+    descricao: "Celular Samsung Galaxy A16,Tela Ampla,Super AMOLED de 6.7",
+    preco: "R$ 792,03",
     categoria: "eletronicos",
     plataforma: "mercado",
-    link: "https://mercadolivre.com/sec/1XipK2W",
-    imagem: "https://http2.mlstatic.com/D_NQ_NP_913995-MLA94997648052_102025-O.webp"
-  },
-  {
-    nome: "iPhone 16 128GB",
-    descricao: "Apple iPhone 16 128GB Preto 5G",
-    preco: "R$ 3.450,00",
-    categoria: "eletronicos",
-    plataforma: "magalu",
-    link: "https://www.magazineluiza.com.br/apple-iphone-16-128gb-preto-6-1-48mp-ios-5g/p/238803400/te/ip16/",
-    imagem: "https://m.magazineluiza.com.br/a-static/420x420/apple-iphone-16-128gb-preto-61-48mp-ios-5g/magazineluiza/238803400/fb05ee53628b697809bce2ba5f5d567e.jpg"
+    link: "https://mercadolivre.com/sec/2vYZZzW",
+    imagem: "https://http2.mlstatic.com/D_NQ_NP_707658-MLA96866257617_102025-O.webp"
   }
 ];
 
